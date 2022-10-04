6a2d5599871fddde045bc260fc11eb1f668d1af3
cats <philip94@snu.ac.kr>
2022-10-04 20:40:39 +0900

    JSON download test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 window.Buffer = require('buffer/').Buffer; 
-
 var JSONStream = require('JSONStream');
+
 // var stream = require('stream-browserify');
 // var test = new stream();
 // console.log(test.toWeb);
@@ -48,10 +48,17 @@ var JSONStream = require('JSONStream');
 
 
 // // console.log(request({url: 'https://jsonplaceholder.typicode.com/todos/1'}))
+fetch('https://main--profound-quokka-7e0251.netlify.app/data/components.json')
+  .then(response => console.log(response.body))
+  .catch(console.log);
 
 fetch('https://jsonplaceholder.typicode.com/todos/1')
   .then(response => console.log(response.body))
   .catch(console.log);
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then(response => console.log(response.body))
+//   .catch(console.log);
   // .then(body => {
   //   const reader = body.getReader();
   //   console.log(reader);
